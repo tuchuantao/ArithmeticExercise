e2d55eb9cc7a9b3f8b988c662ca9ffeb489564a0
tuchuantao <tuchuantao@xiaomi.com>
2018-05-14 14:40:45 +0800

    大数相减

--- arithmetic/src/main/java/com/kevin/arithmetic/Solution3.java
+++ arithmetic/src/main/java/com/kevin/arithmetic/Solution3.java
@@ -3,27 +3,27 @@ package com.kevin.arithmetic;
 public class Solution3 {
 
     public static void main(String[] args) {
-        String result = solution("10 10 11 12 12 11 16");
+        String result = solution("1231231237812739878951331231231237812739878951331231231237812739878951331230000000000000000000000001 - 331231231237812739878951331231231");
         System.out.print(result);
     }
 
     private static String solution(String line) {
-        String[] array = line.split(" ");
-        java.util.HashMap<String, Integer> mapData = new java.util.HashMap<>(array.length / 2 + 1);
+        String[] array = line.split(" - ");
         java.util.ArrayList<String> numberOneList = new java.util.ArrayList<>();
         java.util.ArrayList<String> numberTwoList = new java.util.ArrayList<>();
         for (int i = 0; i < array.length; i++) {
-            for (int j = 0; j < array[i].length(); ) {
-                String num = array[i].subSequence(j, j + 9).toString();
+            int length = array[i].length();
+            for (int j = length; j >= 0; ) {
+                String num = array[i].subSequence(j - 9 >= 0 ? j - 9 : 0, j).toString();
                 if (i == 0) {
                     numberOneList.add(num);
                 } else {
                     numberTwoList.add(num);
                 }
-                j += 9;
+                j -= 9;
             }
         }
-        for (int i = 0; i < numberOneList.size() - 1; i++) {
+        for (int i = 0; i < numberOneList.size(); i++) {
             if (i < numberTwoList.size()) {
                 int numberOne = Integer.valueOf(numberOneList.get(i));
                 int numberTwo = Integer.valueOf(numberTwoList.get(i));
@@ -32,17 +32,38 @@ public class Solution3 {
                     String resultStr = "000000000" + String.valueOf(result);
                     numberOneList.set(i, resultStr.substring(resultStr.length() - 9, resultStr.length()));
                 } else {
-                    int numberOneNext = Integer.valueOf(numberOneList.get(i + 1));
-                    if (numberOneNext > 0)
+                    int j = i + 1;
+                    int numberOneNext = 0;
+                    for (; j < numberOneList.size(); j++) {
+                        numberOneNext = Integer.valueOf(numberOneList.get(j));
+                        if (numberOneNext > 0) {
+                            break;
+                        }
+                    }
+
+                    numberOneNext -= 1;
+                    String numberOneNextStr = "000000000" + String.valueOf(numberOneNext);
+                    numberOneList.set(j, numberOneNextStr.substring(numberOneNextStr.length() - 9, numberOneNextStr.length()));
+
+                    result += 1000000000;
+                    String resultStr = "000000000" + String.valueOf(result);
+                    numberOneList.set(i, resultStr.substring(resultStr.length() - 9, resultStr.length()));
+                    if (j - i > 1) {
+                        for (int k = i + 1; k < j; k++) {
+                            numberOneList.set(k, "999999999");
+                        }
+                    }
                 }
             } else {
                 break;
             }
         }
         StringBuilder result = new StringBuilder();
-        for ()
+        for (int i = numberOneList.size() - 1; i >= 0; i--) {
+            result.append(numberOneList.get(i));
+        }
 
-            // 返回处理后的结果
-            return result;
+        // 返回处理后的结果
+        return result.toString();
     }
 }
