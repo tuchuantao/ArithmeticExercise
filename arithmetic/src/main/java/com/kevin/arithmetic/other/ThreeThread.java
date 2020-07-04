310bfc010e30ed6ba986def88574e6bd4ccbc476
tuchuantao <tuchuantao@xiaomi.com>
2020-07-04 09:59:56 +0800

    update gitignore

--- arithmetic/src/main/java/com/kevin/arithmetic/other/ThreeThread.java
+++ arithmetic/src/main/java/com/kevin/arithmetic/other/ThreeThread.java
@@ -13,7 +13,8 @@ public class ThreeThread {
 
     public static void main(String[] args) {
         //new Solution().solutionFun(3, 100);
-        new Solution().solutionFun3(3, 100);
+        //new Solution().solutionFun3(3, 100);
+        new Solution().threadPrint(3, 100);
     }
 }
 
@@ -22,7 +23,7 @@ class Solution {
     private volatile int number = 1;
 
     /**
-     * 使用LOCk
+     * 使用Lock
      * <p>
      * 三个线程通过 signalAll() 和 await()
      * 并不是最优解，存在一个试错的过程，
@@ -119,6 +120,70 @@ class Solution {
                     }
                 } catch (InterruptedException e) {
                     e.printStackTrace();
+                }
+            }).start();
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    public void threadPrint(int threadCount, int maxNumber) {
+        Semaphore[] arrays = new Semaphore[threadCount];
+        for (int i = 0; i < threadCount; i++) {
+            arrays[i] = new Semaphore(1);
+            if (i > 0) {
+                try {
+                    arrays[i].acquire();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        for (int i = 1; i <= threadCount; i++) {
+            int index = i % threadCount;
+            new Thread(() -> {
+                while (number <= maxNumber) {
+                    if (number % threadCount == index) {
+                        try {
+                            arrays[index - 1 >= 0 ? index - 1 : threadCount - 1].acquire();
+                            System.out.println("Thread :" + index + "  number=" + number);
+                            number++;
+                            arrays[index].release();
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
                 }
             }).start();
         }
