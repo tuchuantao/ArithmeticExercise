25bbb707bb6df702250a6add93b9bb742da91c6e
tuchuantao <tuchuantao@kuaishou.com>
2021-11-22 14:43:13 +0800

    Offer33:二叉搜索树的后序遍历序列

--- arithmetic/src/main/java/com/kevin/arithmetic/leetcode/Offer33.java
+++ arithmetic/src/main/java/com/kevin/arithmetic/leetcode/Offer33.java
@@ -60,6 +60,18 @@ public class Offer33 {
     return result;
   }
 
+  public boolean verifyPostorder1(int[] postorder) {
+    return recur(postorder, 0, postorder.length - 1);
+  }
+  boolean recur(int[] postorder, int i, int j) {
+    if(i >= j) return true;
+    int p = i;
+    while(postorder[p] < postorder[j]) p++;
+    int m = p;
+    while(postorder[p] > postorder[j]) p++;
+    return p == j && recur(postorder, i, m - 1) && recur(postorder, m, j - 1);
+  }
+
   public boolean verifyPostorder2(int[] postorder) {
     Stack<Integer> stack = new Stack<>();
     int root = Integer.MAX_VALUE;
