3e7cdf3c1b59bbe0b1cf6976f8a61233f64bd25a
tuchuantao <tuchuantao@kuaishou.com>
2022-01-05 15:50:21 +0800

    2017:网格游戏

--- arithmetic/src/main/java/com/kevin/arithmetic/util/Utils.java
+++ arithmetic/src/main/java/com/kevin/arithmetic/util/Utils.java
@@ -34,10 +34,11 @@ public class Utils {
   }
 
   public static void printMulArr(int[][] arr) {
-    int len = arr.length;
-    for (int i = 0; i < len; i++) {
+    int row = arr.length;
+    int col = arr[0].length;
+    for (int i = 0; i < row; i++) {
       System.out.print("[");
-      for (int j = 0; j < len; j++) {
+      for (int j = 0; j < col; j++) {
         System.out.print(arr[i][j] + ",");
       }
       System.out.println("]");
